8a1f273ab72945b7fc72029d18edc2b7980a3630
hrushalipatil <hrushalipatil21@gmail.com>
2024-04-20 21:19:41 +0530

    first commit

--- js/day34.js
+++ js/day34.js
@@ -1,24 +1,62 @@
-// Essenatials -- for starting react
+// // Essenatials -- for starting react
 
 
-// //destruccting of objects and array 
+// // //destruccting of objects and array 
 
-//arry
-let name = ["vrushali","kabir", "sayali"]
+// //arry
+// let name = ["vrushali","kabir", "sayali"]
 
 
-let [a,b,c]=name
+// let [a,b,c]=name
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
 
-//object
+// //object
 
-let Info ={
-    firstName :"vrushali",
-    lastName : "patil"
+// let Info ={
+//     firstName :"vrushali",
+//     lastName : "patil"
 
-}
+// }
+
+let students = [
+
+    {
+        firstname: "chinmay",
+        lastName: "deshpande"
+    },
+    {
+        firstname: "poorva",
+        lastName: "vyas"
+    }
+    ,
+    {
+        firstname: "shivani",
+        lastName: "headua"
+    }
+]
+
+// students.forEach(element => {
+//     if(element.firstname == "shivani")
+//     console.log(element.firstname="vrushaLI")
+
+// for (i = 0; i < students.length; i++) {
+//     students[i].city.push("pune")
+//     console.log(students[i])
+// }
+
+
+students.forEach(function(el){
+    students.push(el.city="pune")
+    console.log(el)
+
+})
+// console.log(A)
+
+// { firstname: 'chinmay', lastName: 'deshpande', city: 'pune' }
+// { firstname: 'poorva', lastName: 'vyas', city: 'pune' }
+// { firstname: 'shivani', lastName: 'headua', city: 'pune' }
 
 
