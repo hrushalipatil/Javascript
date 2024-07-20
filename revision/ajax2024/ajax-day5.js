d93e8d6cde4cdf166594933996a3f751a03e750a
hrushalipatil <hrushalipatil21@gmail.com>
2024-07-20 14:33:07 +0530

    added

--- revision/ajax2024/ajax-day5.js
+++ revision/ajax2024/ajax-day5.js
@@ -168,3 +168,93 @@ async function DeleteUser(){
 }
 
 deleteUser(2)
+
+
+// -------------------------------------------------
+
+function getUser(id) {
+    return fetch(`https://reqres.in/api/users/${id}`)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (res) {
+            return res.data
+        })
+}
+function getUserInfo() {
+   return  new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            reject("Bye")
+        }, 2000)
+    })
+}
+
+//Promise combinators
+
+//Promise.all()  ==> shortcircut at reject state
+
+// async function getUsers() {
+//     try {
+//         let a = await Promise.all([
+//             getUser(1),
+//             getUser(2),
+//             getUser(3),
+//             getUserInfo()
+//         ])
+//         console.log(a)
+//     }
+//     catch {
+//         console.log("error")
+//     }
+// }
+// getUsers()
+
+// Promise.allsettle() => executed for both resolve and reject
+
+// async function getAlluser(){
+//     let a=await Promise.allSettled([
+//         getUser(1),
+//         getUserInfo(),
+//         getUser(2)
+//     ])
+//     console.log(a)
+// }
+// getAlluser()
+// : 
+// {status: 'fulfilled', value: {…}}
+// 1
+// : 
+// {status: 'rejected', reason: 'Bye'}
+// 2
+// : 
+// {status: 'fulfilled', value: {…}}
+
+//Promise.race() => whoever reched first give resonse
+ 
+// async function getAlluser(){
+//     try{
+//         let a=await Promise.race([
+//             getUser(1),
+//             getUserInfo(),
+//             getUser(2),
+//         ])
+//         console.log(a) 
+//     }
+//   catch{
+//     console.log("Error")
+//   }
+
+// }
+// getAlluser()
+
+//Promise.any()=> short circuit at first resolve state
+
+async function getAlluser(){
+    let a=await Promise.any([
+        
+        getUserInfo(),
+        getUser(2)
+    ])
+    console.log(a)
+}
+getAlluser()
